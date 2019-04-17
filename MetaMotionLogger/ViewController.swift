e8f7fef64241a7f871705f453832c153c4349db4
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2019-04-17 03:29:56 +0200

    Initial Commit

---
+++ MetaMotionLogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetaMotionLogger
+//
+//  Created by Shoya Ishimaru on 2019.04.17.
+//  Copyright © 2019 shoya140. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
