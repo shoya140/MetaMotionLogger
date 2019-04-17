b99c1a24b3794facac19b7c315d4b58b87c277f1
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2019-04-17 04:32:03 +0200

    Pod install Bolts-Swift and added LED example

--- MetaMotionLogger/ViewController.swift
+++ MetaMotionLogger/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import MetaWear
+import MetaWearCpp
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        MetaWearScanner.shared.startScan(allowDuplicates: true) { (device) in
+            // We found a MetaWear board, see if it is close
+            if device.rssi > -50 {
+                // Hooray! We found a MetaWear board, so stop scanning for more
+                MetaWearScanner.shared.stopScan()
+                // Connect to the board we found
+                device.connectAndSetup().continueWith { t in
+                    if let error = t.error {
+                        // Sorry we couldn't connect
+                        print(error)
+                    } else {
+                        // Hooray! We connected to a MetaWear board, so flash its LED!
+                        var pattern = MblMwLedPattern()
+                        mbl_mw_led_load_preset_pattern(&pattern, MBL_MW_LED_PRESET_PULSE)
+                        mbl_mw_led_stop_and_clear(device.board)
+                        mbl_mw_led_write_pattern(device.board, &pattern, MBL_MW_LED_COLOR_GREEN)
+                        mbl_mw_led_play(device.board)
+                    }
+                }
+            }
+        }
     }
 
-
 }
-
