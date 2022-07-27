a90c119a352774e378aa8d3bd765b634809510a0
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2022-07-27 23:30:37 +0200

    init

---
+++ Shared/MetaMotionLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMotionLoggerApp.swift
+//  Shared
+//
+//  Created by Shoya Ishimaru on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct MetaMotionLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
